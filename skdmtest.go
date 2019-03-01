58fd0272131d1d77169ca61b9effcb291ff59789
ekstyle <ekstyle@yandex.ru>
2019-03-01 11:35:07 +0500

    V1 branch test

--- skdmtest.go
+++ skdmtest.go
@@ -1,7 +1,8 @@
 package skdmtest
+
 import "fmt"
 
 // Hi returns a friendly greeting
 func Hi(name string) string {
-	return fmt.Sprintf("Hi, %s", name)
+	return fmt.Sprintf("Hi, from v1 branch %s", name)
 }
